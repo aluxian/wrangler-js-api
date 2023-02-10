22672128ef4346bfe9cfd4bd0663963cfc712836
Alexandru Rosianu <me@aluxian.com>
2023-02-10 01:07:50 +0000

    re-export Environment

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 import type { ChildProcess } from "child_process";
 import type { Environment } from "./wrangler";
 
+export { Environment } from "./wrangler";
+
 export function publish(config: Environment): ChildProcess;
 
 export function preview(config: Environment): ChildProcess;
